54a80e5d9f00e8fd639a078e8375aca7892b493d
Natalia <n.d.maistrenko@gmail.com>
2019-03-03 15:16:35 +0200

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,7 @@ module.exports = {
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`),
-    publicPath: 'http: //localhost:8080/',
-    hot: true,
+    publicPath: 'http://localhost:8080/',
     compress: true,
-    watchContentBase: true
   }
 };
